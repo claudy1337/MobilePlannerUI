7e00955dcccc384af80dcc0225a66b726298270a
darkroot <45602451+claudy1337@users.noreply.github.com>
2022-03-20 02:40:52 +0300

    fix

--- PropertyApp/PropertyApp/AssemblyInfo.cs
+++ PropertyApp/PropertyApp/AssemblyInfo.cs
@@ -2,8 +2,9 @@ using Xamarin.Forms.Xaml;
 using Xamarin.Forms;
 
 
+
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("Druk.ttf", Alias = "Druk.ttf")]
+[assembly: ExportFont("Fonts/Druk/Druk.ttf", Alias = "Druk.ttf")]
 [assembly: ExportFont("Druk-Wide-Medium.ttf", Alias = "Druk-Wide-Medium.ttf")]
 [assembly: ExportFont("Ubuntu-Light.ttf", Alias = "Ubuntu - Light.ttf")]
 [assembly: ExportFont("Ubuntu-Medium.ttf", Alias = "Ubuntu-Medium.ttf")]
